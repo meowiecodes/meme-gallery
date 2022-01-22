a115b6c22ee678aac846d8eef760cf02850bba80
meowiecodes <86108768+meowiecodes@users.noreply.github.com>
2022-01-23 05:37:56 +0800

    added the updateGallery function

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-var imageUrls = ['https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg', 'https://etcanada.com/wp-content/uploads/2016/06/58c0500b2ac4c342c6fc4ab096ada150.jpg?quality=80&strip=all&crop=0px%2C21px%2C500px%2C333px&resize=720%2C480',
-  'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'];
+var imageUrls = [];
 
 var gallery = document.querySelector('main');
 var imageUrlInput = document.querySelector('input');
@@ -10,6 +9,7 @@ addImageButton.addEventListener('click', function (event) {
     imageUrls.push(imageUrlInput.value);
   }
   imageUrlInput.value = '';
+  updateGallery();
 });
 
 function updateGallery() {
